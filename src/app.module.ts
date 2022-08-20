41bc112c7c119894fb4a8672cee031bbde0a24d5
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-21 00:17:30 +0900

    feat : typeorm 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule]
+  imports: [TypeOrmModule.forRoot(typeORMConfig),
+    UserModule]
 })
 export class AppModule {}
