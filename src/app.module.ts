5b28abdc5d3fb6274699b8120448524e228aaec9
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-21 00:27:45 +0900

    feat : company module 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig),
-    UserModule]
+    UserModule,
+    CompanyModule]
 })
 export class AppModule {}
