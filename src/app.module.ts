a8c428aec5ad98b182ab1b7e2ce6898e65662673
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-21 00:32:17 +0900

    feat : post module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { UserModule } from './user/user.module';
 import { CompanyModule } from './company/company.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig),
     UserModule,
-    CompanyModule]
+    CompanyModule,
+    PostModule]
 })
 export class AppModule {}
